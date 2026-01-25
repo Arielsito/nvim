17d655e8d46bd67767c28bb8176c23b85cddb3c7
Arielsito <ang14012006@gmail.com>
2026-01-25 13:25:41 -0600

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "python",
+        "html",
+        "css",
+        "javascript",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+      },
+
+      sync_install = false,
+      auto_install = true,
+      autopairs = { enable = true },
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = true },
+    })
+  end
+}
