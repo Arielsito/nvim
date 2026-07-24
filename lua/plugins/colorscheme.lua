8e6b0468fa0e87999a6c1de76da2a6208c4fe6b9
Arielsito <ang14012006@gmail.com>
2026-07-24 12:12:21 -0600

    add dynamic colorschemes based on wp

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,57 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "night",
-    transparent = false,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = false,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
-  },
-  keys = {
-    {
-      "<leader>bt",
-      function()
-        local conf = require("tokyonight.config")
-        conf.options.transparent = not conf.options.transparent
-        require("tokyonight").setup(conf.options)
-        vim.cmd("colorscheme tokyonight")
-      end,
-      desc = "Toggle background transparency",
+    keys = {
+      {
+        "<leader>bt",
+        function()
+          local conf = require("tokyonight.config")
+          conf.options.transparent = not conf.options.transparent
+          require("tokyonight").setup(conf.options)
+          vim.cmd("colorscheme tokyonight")
+        end,
+        desc = "Toggle background transparency",
+      },
     },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end
   },
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd.colorscheme("tokyonight")
-  end
-  -- Dynamic colorscheme based on wallpaper
-  -- "uZer/pywal16.nvim",
-  -- config = function()
-  --   vim.cmd.colorscheme("pywal16")
-  -- end
+  {
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local wal_cache = vim.fn.expand("~/.cache/wal/colors.json")
+      local has_wal = vim.fn.filereadable(wal_cache) == 1
+
+      local applied_neopywal = false
+
+      if has_wal then
+        local ok = pcall(function()
+          require("neopywal").setup({
+            transparent_background = true,
+            no_bold = true,
+          })
+          vim.cmd.colorscheme("neopywal")
+        end)
+        applied_neopywal = ok
+      end
+
+      if not applied_neopywal then
+        vim.cmd.colorscheme("tokyonight")
+      end
+    end,
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,86 +47,130 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = {
-			options = {
-				theme = "auto",
-				component_separators = "",
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = { "alpha", "Outline" },
-			},
+    config = function()
+      local function build_theme()
+        local current_theme = vim.g.colors_name or ""
+        local lualine_theme = "auto"
+        if current_theme:find("neopywal") then
+          local ok, C = pcall(function() return require("neopywal").get_colors() end)
+          if ok and C then
+            local dark_bg = C.background or C.color0 or "#000000"
+            -- local U = require("neopywal.utils.color") -- in case you want to mess with the colors
+            lualine_theme = {
+              normal = {
+                a = { fg = dark_bg, bg = C.color6, gui = "none" },
+                b = { fg = C.foreground, bg = C.color0, gui = "none" },
+                c = { fg = dark_bg, bg = C.color3, gui = "none" },
+                x = { fg = dark_bg, bg = C.color3, gui = "none" },
+                y = { fg = C.foreground, bg = C.color0, gui = "none" },
+                z = { fg = dark_bg, bg = C.color6, gui = "none" },
+              },
+              insert = {
+                a = { fg = dark_bg, bg = C.color2, gui = "none" },
+                z = { fg = dark_bg, bg = C.color2, gui = "none" },
+              },
+              visual = {
+                a = { fg = dark_bg, bg = C.color5, gui = "none" },
+                z = { fg = dark_bg, bg = C.color5, gui = "none" },
+              },
+              replace = {
+                a = { fg = dark_bg, bg = C.color1, gui = "none" },
+                z = { fg = dark_bg, bg = C.color1, gui = "none" },
+              },
+              command = {
+                a = { fg = dark_bg, bg = C.color3, gui = "none" },
+                z = { fg = dark_bg, bg = C.color3, gui = "none" },
+              },
+              inactive = {
+                a = { fg = C.color8, bg = "NONE", gui = "none" },
+                b = { fg = C.color8, bg = "NONE", gui = "none" },
+                c = { fg = C.color8, bg = "NONE", gui = "none" },
+              },
+            }
+          end
+        end
+        return lualine_theme
+      end
 
-			sections = {
-				lualine_a = {
-					{
-						"mode",
-						separator = { left = "", right = "" },
-						icon = "󱔎",
-					},
-				},
-				lualine_b = {
-					{
-						"filetype",
-            fmt = string.upper,
-						icon_only = true,
-            padding = { left = 2, right = 0 },
-					},
-					"filename",
-				},
-				lualine_c = {
-					{
-						"branch",
-						icon = "",
+      local function build_opts()
+        return {
+          options = {
+            theme = build_theme(),
+            component_separators = "",
+            disabled_filetypes = { "alpha", "Outline" },
           },
-					{
-						"diff",
-						colored = true,
-						symbols = { added = " ", modified = " ", removed = " " },
-            diff_color = {
-              added = { fg = "#A6E22E" },
-              modified = { fg = "#66D9EF" },
-              removed = { fg = "#F92672" },
+          sections = {
+            lualine_a = {
+              {
+                "mode",
+              },
             },
-					},
-				},
-				lualine_x = {
-					{
-						"diagnostics",
-						symbols = { error = " ", warning = " ", info = " ", hint = " " },
-						update_in_insert = true,
-					},
-				},
-				lualine_y = {
-					{
-						function()
-							local bufnr = vim.api.nvim_get_current_buf()
+            lualine_b = {
+              {
+                "diagnostics",
+                symbols = { error = " ", warning = " ", info = " ", hint = " " },
+                update_in_insert = true,
+                always_visible = true,
+              },
+            },
+            lualine_c = {
+              "filename",
+            },
+            lualine_x = {
+              {
+                function()
+                  local bufnr = vim.api.nvim_get_current_buf()
 
-							local clients = vim.lsp.get_clients()
-							if next(clients) == nil then
-								return ""
-							end
+                  local clients = vim.lsp.get_clients()
+                  if next(clients) == nil then
+                    return ""
+                  end
 
-							local c = {}
-							for _, client in pairs(clients) do
-								table.insert(c, client.name)
-							end
-							return " " .. table.concat(c, "  ")
-						end,
-					},
-				},
-				lualine_z = {
-					{ "location", separator = { left = "", right = "" }, icon = "" },
-				},
-			},
-			inactive_sections = {
-				lualine_a = { "filename" },
-				lualine_b = { "" },
-				lualine_c = { "" },
-				lualine_x = { "" },
-				lualine_y = { "" },
-				lualine_z = { "location" },
-			},
-		},
-  },
+                  local c = {}
+                  for _, client in pairs(clients) do
+                    table.insert(c, client.name)
+                  end
+                  return " " .. table.concat(c, " | ")
+                end,
+              },
+            },
+            lualine_y = {
+              {
+                "branch",
+                icons_enabled = false,
+              },
+              {
+                "diff",
+                colored = true,
+                symbols = { added = " ", modified = " ", removed = " " },
+              },
+            },
+            lualine_z = {
+              { "location" },
+            },
+          },
+          inactive_sections = {
+            lualine_a = { "filename" },
+            lualine_b = { "" },
+            lualine_c = { "" },
+            lualine_x = { "" },
+            lualine_y = { "" },
+            lualine_z = { "location" },
+          },
+        }
+      end
+
+      require("lualine").setup(build_opts())
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*neopywal*",
+        callback = function()
+          vim.schedule(function()
+            require("lualine").setup(build_opts())
+          end)
+        end
+      })
+    end,
+   },
   -- vim-tmux-navigator
   {
     "christoomey/vim-tmux-navigator",
